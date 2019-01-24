b42d13a06b6d24d44e6512d5165d36c3ff42ff1f
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-24 21:18:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,11 @@ class ApplicationController < Sinatra::Base
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
       session[:id] = @user.id
-
-    redirect '/users/home'
+    if @user 
+      redirect '/users/home'
+    else 
+      redirect '/sessions/login'
+    end 
   end
 
   get '/sessions/logout' do 
